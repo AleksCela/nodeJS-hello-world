1c18aede2fa2b30c33fd4e3227447a89a408fb00
Aleks Cela <alekscela7@gmail.com>
2022-11-30 15:18:49 +0100

    Done

--- index.js
+++ index.js
@@ -1,12 +1,27 @@
-const http = require('http');
-
+import http from 'http';
+import fsp from "fs/promises";
 const hostname = '127.0.0.1';
 const port = 3000;
 
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World');
+const server = http.createServer(async (req, res) => {
+    // res.statusCode = 200;
+    // res.setHeader('Content-Type', 'text/html');
+    if (req.url === '/') {
+        const content = await fsp.readFile('./index.html', 'utf-8')
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/html');
+        res.end(content)
+    } else if (req.url === '/about') {
+        const content = await fsp.readFile('./about.html', 'utf-8')
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/html');
+        res.end(content)
+    } else {
+
+        res.statusCode = 404;
+        res.setHeader('Content-Type', 'text/plain');
+        res.end("Not found")
+    }
 });
 
 server.listen(port, hostname, () => {
