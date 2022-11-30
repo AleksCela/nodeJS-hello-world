4be391f4a1349ad34912b49c9fce0f18ecc61408
Aleks Cela <alekscela7@gmail.com>
2022-11-30 17:07:48 +0100

    Done v.2

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 #My first node server
+
+This is the solution of the following excercise:
+
+Write a server that:
+- given a file data.csv containing 1 number for each line
+- if client requests /unique then it returns the unique values in the file
+- if client requests /sum then it returns the sum of all the values in the file
--- index.js
+++ index.js
@@ -1,17 +1,27 @@
 import express from 'express'
 import fsp from "fs/promises";
+import sumOfArray from "./functions.js";
 const app = express()
 const port = 3000
 const hostname = '127.0.0.1';
 
-app.get('/', async (req, res) => {
-    const content = await fsp.readFile('./index.html', 'utf-8')
-    res.send(content)
+app.get('/unique', async (req, res) => {
+    const content = await fsp.readFile('./file.csv', 'utf-8')
+    const array = content.split("\r\n")
+    const unique = array.filter((item, index, arr) => arr.indexOf(item) === index)
+    console.log(unique);
+    res.send("the unique values of " + content + " are " + unique)
 })
 
-app.get('/about', async (req, res) => {
-    const content = await fsp.readFile('./about.html', 'utf-8')
-    res.send(content)
+app.get('/sum', async (req, res) => {
+    const content = await fsp.readFile('file.csv', 'utf-8')
+    const array = content.split("\r\n")
+    const arrayOfNum = []
+    for (const number of array) {
+        arrayOfNum.push(Number(number))
+    }
+    const theSum = sumOfArray(arrayOfNum)
+    res.send(content + " The sum => " + theSum)
 })
 
 app.listen(port, () => {
